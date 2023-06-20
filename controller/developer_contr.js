fcdb9f0995cca90436acbf7619548683788aaf22
Osamuyi <muyixone@yahoo.com>
2023-06-20 03:54:53 +0100

    write the logic for the create user controller

--- controller/developer_contr.js
+++ controller/developer_contr.js
@@ -1,5 +1,29 @@
-const createUser = (req, res) => {
-  res.send('User created successfully');
-};
+const { generateApiKey } = require('generate-api-key');
+const bcrypt = require('bcryptjs');
+
+const developer = require('../models/developer_schema');
+const asyncWrapper = require('../middlewares/asyncWrapper');
+
+const createUser = asyncWrapper(async (req, res) => {
+  const { body: data } = req;
+  const { name, email, password } = data;
+  const apiKey = generateApiKey({ method: 'uuidv4' });
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const hashedApiKey = await bcrypt.hash(apiKey, salt);
+
+  const user = new developer({
+    name,
+    email,
+    password: hashedPassword,
+    apiKey: hashedApiKey,
+  });
+
+  await user.save();
+  console.log(apiKey);
+
+  res.status(200).json({});
+});
 
 module.exports = { createUser };
