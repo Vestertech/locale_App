ac31af5e9cab3aee0886fdaa0d79e144f3688350
Osamuyi <muyixone@yahoo.com>
2023-06-28 01:52:07 +0100

    Create the getAllStates function with search parameters

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Locale_API
 
 A developer's API that provides geographical information about Nigeria. This API shows you all of Nigeria's regions, states and local government areas. You can also get information about a specific region or state.
+
+### Get All States
+
+To select specific fields from the result,
+example
+domainName/api/v1/location/states?fields=stateName,state_metadata
+
+To get specific number of documents from the result
+domainName/api/v1/location/states?limit=2
+
+To get a specific page
+domainName/api/v1/location/states?page=1
+
+To filter the states on a specific numerical conditions like the population
--- controller/locate-states.js
+++ controller/locate-states.js
@@ -1,14 +1,91 @@
+const { populate } = require('../models/developer_schema');
 const StateModel = require('../models/states');
 
 const getAllStates = async (req, res) => {
   try {
-    const states = await StateModel.find({
-      _id: '649a38b8c9346a8e6e325d4e',
-    }).populate('region');
+    const { numericFilters, sort, stateName, LGA, fields } = req.query;
+    const queryObject = {};
 
-    return console.log(spreadOut);
+    if (stateName) {
+      queryObject.stateName = { $regex: stateName, $options: 'i' };
+    }
+    if (LGA) {
+      queryObject.LGA = { $regex: LGA, $options: 'i' };
+    }
 
-    res.status(200).json({ states });
+    // Search states by population &&/|| numberOFLAGs
+    if (numericFilters) {
+      // Map the conditional signs with their relative mongoose equivalents
+      // Store the signs in a regex so we can compare them
+      // if (numericFilters === any Regex pattern)
+      // Replace the pattern with the key of the operatorMap
+      const operatorMap = {
+        '>': '$gt',
+        '>=': '$gte',
+        '=': '$eq',
+        '<': '$lt',
+        '<=': '$lte',
+      };
+
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+      let filters = numericFilters.replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      );
+      let statePopulationField = await StateModel.find({});
+      let getPopulation = statePopulationField.forEach((pop) => {
+        return pop.state_metadata;
+      });
+
+      // Set fields with numerical values to search by
+      const options = ['population', 'numberOfLGAs'];
+      filters = filters.split(',').forEach((item) => {
+        const [field, operator, value] = item.split('-');
+        if (options.includes(field)) {
+          queryObject[field] = { [operator]: Number(value) };
+        }
+      });
+    }
+
+    let result = StateModel.find(queryObject);
+
+    // SORT
+    if (sort) {
+      // If more than one key is specified in the query object,
+      // the string should be seperated by a space
+      // so that the result is can sort each independently
+
+      const sortList = sort.split(',').join(' ');
+
+      // result.state_metadata = result.sort(sortList);
+      result = result.sort(sortList);
+    }
+    //  else {
+    //   result.state_metadata['population'] = result.sort(
+    //     `state_metadata[population]`
+    //   );
+    // }
+
+    // Return specific fields in the result
+    if (fields) {
+      const fildsList = fields.split(',').join(' ');
+      result = result.select(fildsList);
+    } else {
+      // Exclude the region and local government areas field except explicitly specified
+      result = result.select('-region -LGA');
+    }
+
+    // Pagination
+    const page = Number(req.query.page) || 1;
+    const docsPerPage = 3;
+    const limit = Number(req.query.limit) || docsPerPage;
+    const skip = limit * (page - 1);
+
+    result = result.skip(skip).limit(limit);
+
+    const states = await result;
+
+    res.status(200).json({ states, nbHits: states.length });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
