bf4e5c959effab8253e8815b24b44c166bf5457c
Osamuyi <muyixone@yahoo.com>
2023-06-30 04:27:29 +0100

    Add the function to enable res.json and console.log functions enable virtuals

--- models/regions.js
+++ models/regions.js
@@ -2,32 +2,38 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Schema for Region
-const RegionSchema = Schema({
-  regionName: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  metadata: {
-    coordinates: {
-      type: {
-        latitude: {
-          type: Number,
-        },
-        longitude: {
-          type: Number,
+const RegionSchema = Schema(
+  {
+    regionName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    metadata: {
+      coordinates: {
+        type: {
+          latitude: {
+            type: Number,
+          },
+          longitude: {
+            type: Number,
+          },
         },
       },
     },
+    size: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    population: {
+      type: mongoose.Schema.Types.Mixed,
+    },
   },
-  size: {
-    type: mongoose.Schema.Types.Mixed,
-  },
-  population: {
-    type: mongoose.Schema.Types.Mixed,
-  },
-});
+  {
+    toJSON: { virtuals: true }, //So `res.json()` and other `JSON.stringify()` functions include virtuals
+    toObject: { virtuals: true }, //So `console.log()` and other functions that use `toObject()` include virtuals
+  }
+);
 
 // In order for one to be able to populate a list of all
 // states in a region, use the virtual function
@@ -35,8 +41,6 @@ RegionSchema.virtual('states', {
   ref: 'State',
   localField: '_id',
   foreignField: 'region',
-  toJSON: { virtuals: true }, //So `res.json()` and other `JSON.stringify()` functions include virtuals
-  toObject: { virtuals: true }, //So `console.log()` and other functions that use `toObject()` include virtuals
 });
 
 module.exports = mongoose.model('Region', RegionSchema);
