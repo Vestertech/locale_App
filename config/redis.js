f38df792d427fc4e6e63c4ca9d2b6be7c8303240
Osamuyi <muyixone@yahoo.com>
2023-07-02 00:04:09 +0100

    Define a redis class for caching

---
+++ config/redis.js
@@ -0,0 +1,28 @@
+const Redis = require('redis');
+
+class Cache {
+  constructor() {
+    this.redis = null;
+  }
+
+  async connect() {
+    try {
+      this.redis = await Redis.createClient();
+
+      this.redis.connect();
+
+      this.redis.on('connect', () => {
+        console.log('Redis connected');
+      });
+
+      this.redis.on('error', () => {
+        console.log('Redis connection error');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const instance = new Cache();
+module.exports = instance;
