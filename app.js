4042b6605bbb7ce5604eb7af7dc84a30d661ceb4
Osamuyi <muyixone@yahoo.com>
2023-07-06 02:52:59 +0100

    Add Helmet

--- app.js
+++ app.js
@@ -26,11 +26,8 @@ const app = express();
 app.use(rateLimit);
 app.use(express.json());
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
 
-app.use('/', (req, res) => {
-  res.status(200).json({ message: 'Locale API' });
-});
 app.use('/api/v1/developer', developerRoute);
 app.use('/api/v1/create', jwtAuthentication, createRoute);
 app.use('/api/v1/location', authenticateKey, cacheMiddleware, locationRoute);
